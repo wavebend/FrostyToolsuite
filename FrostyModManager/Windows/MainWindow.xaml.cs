b80d322e8ad82094d2d2bbad070797aef4a39c53
Joel <52287493+J-Lyt@users.noreply.github.com>
2025-04-15 21:19:14 +0100

    [FrostyModManager] Show replacements only by default when selecting the 'Actions' tab.

--- FrostyModManager/Windows/MainWindow.xaml.cs
+++ FrostyModManager/Windows/MainWindow.xaml.cs
@@ -1733,7 +1733,7 @@ namespace FrostyModManager
         {
             if (conflictsTabItem.IsSelected)
             {
-                UpdateConflicts();
+                showOnlyReplacementsCheckBox.IsChecked = true;
             }
         }
 
@@ -1849,6 +1849,23 @@ namespace FrostyModManager
 
             // Re-run filter since we might be filtering on applied mods.
             RefreshFilter();
+        }
+
+        private void PART_ShowOnlyReplacementsCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = FrostyMessageBox.Show("Are you sure you want to show all resources? This can take some time.", "Resources", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                UpdateConflicts();
+
+                // Re-run filter since we might be filtering on applied mods.
+                RefreshFilter();
+            }
+            else
+            {
+                showOnlyReplacementsCheckBox.IsChecked = true;
+            }
         }
 
         private void optionsMenuItem_Click(object sender, RoutedEventArgs e)
