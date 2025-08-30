8d257305dad5e24b4838695ef1764113303ea20b
Joel <52287493+J-Lyt@users.noreply.github.com>
2025-08-30 05:22:05 +0100

    [FrostyModManager] Fixed issue where manager could crash when checking resource conflicts
    - If a .fbmod file was missing, it would still be checked for conflicts if it was enabled within the Applied mods list.
    - It will now skip any .fbmod files that are missing or not enabled.

--- FrostyModManager/Windows/MainWindow.xaml.cs
+++ FrostyModManager/Windows/MainWindow.xaml.cs
@@ -1801,7 +1801,7 @@ namespace FrostyModManager
                     for (int i = 0; i < selectedPack.AppliedMods.Count; i++)
                     {
                         FrostyAppliedMod appliedMod = selectedPack.AppliedMods[i];
-                        if (!appliedMod.IsFound && !appliedMod.IsEnabled)
+                        if (!appliedMod.IsFound || !appliedMod.IsEnabled)
                             continue;
 
 
@@ -1923,7 +1923,7 @@ namespace FrostyModManager
             for (int i = 0; i < selectedPack.AppliedMods.Count; i++)
             {
                 FrostyAppliedMod appliedMod = selectedPack.AppliedMods[i];
-                if (!appliedMod.IsFound && !appliedMod.IsEnabled)
+                if (!appliedMod.IsFound || !appliedMod.IsEnabled)
                     continue;
 
                 Binding primaryActionBinding = new Binding("") { Converter = new ModPrimaryActionConverter(), ConverterParameter = appliedMod.Mod.Filename };
