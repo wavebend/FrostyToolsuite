0f2ab678b5c9e02066b158cfcd15c8a2cfec9397
Joel <52287493+J-Lyt@users.noreply.github.com>
2025-09-23 16:30:17 +0100

    [FrostyCore/Editor] Change Version and Update Changelog

--- FrostyPlugin/App.cs
+++ FrostyPlugin/App.cs
@@ -32,7 +32,7 @@ namespace Frosty.Core
 
         public static string Version = "";
         public static readonly int MinorVersion = 1;
-        public static readonly string BuildVersion = "2025.9.15.0";
+        public static readonly string BuildVersion = "2025.9.23.0";
 
         public static string ProfileSettingsPath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Frosty/" + ProfilesLibrary.ProfileName;
         public static string GlobalSettingsPath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Frosty";
